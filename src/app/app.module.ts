59332a1a1f7f380502d8247615b936b2df62cf87
Michael Blanco <89497253+MigueBlanc@users.noreply.github.com>
2022-11-17 22:05:35 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { AngularFireModule } from "@angular/fire/compat";
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'eventapp-1a9b3',
+    appId: '1:149409880537:web:134c936f36be682359e1c8',
+    storageBucket: 'eventapp-1a9b3.appspot.com',
+    apiKey: 'AIzaSyBsEc2BKPdFetduH1LQWRKSHrsdrbIHQoc',
+    authDomain: 'eventapp-1a9b3.firebaseapp.com',
+    messagingSenderId: '149409880537',
+  },
+  production: true
+};
